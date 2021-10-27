b04c9722ed4e26707ca08f1402cbe8fd86c8d959
z3f1r0 <73543172+z3f1r0@users.noreply.github.com>
2021-10-27 11:53:01 +0200

    Update PDF_merger.py

--- PDF_merger.py
+++ PDF_merger.py
@@ -4,23 +4,23 @@ import PyPDF2
 import os
 
 # list of files and directory in the working directory
-listaFiles = listdir()
+listFiles = listdir()
 
 # open a pdf file for writing to merge in all others
 print("How do you want to call your PDF file?\nRemember to add the '.pdf' extension")
-nomeFileDestinazione = input() # name of final pdf file
-PDF_Destinazione = open(nomeFileDestinazione, "wb")  # w=write, b=binary
+nameFileDestination = input() # name of final pdf file
+PDF_Destination = open(nameFileDestination, "wb")  # w=write, b=binary
 merger = PyPDF2.PdfFileMerger() # this is a merger for all pdf files
 
-for nomeFile in listaFiles:
-    if nomeFile.endswith(".pdf"): # choose only pdf file extension
-        pdfFile = open(nomeFile, "rb")  # r=read, b=binary
+for nameFile in listFiles:
+    if nameFile.endswith(".pdf"): # choose only pdf file extension
+        pdfFile = open(nameFile, "rb")  # r=read, b=binary
         readerPDF = PyPDF2.PdfFileReader(pdfFile)
         merger.append(readerPDF)
         pdfFile.close()
 
-merger.write(PDF_Destinazione)
-PDF_Destinazione.close()
+merger.write(PDF_Destination)
+PDF_Destination.close()
 
 
 
